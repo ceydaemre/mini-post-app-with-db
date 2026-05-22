e0ed4a17c35d7dc1b5e98450f6691a2633b08221
Ceyda Emre <ceydaemre1100@gmail.com>
2026-05-22 14:10:37 +0300

    chore: support DATABASE_URL for production database

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,22 @@
 const { Pool } = require("pg");
+
 require("dotenv").config({
   path: require("path").resolve(__dirname, "../../.env")
 });
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || undefined,
-  database: process.env.DB_NAME,
-});
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD || undefined,
+      database: process.env.DB_NAME,
+    });
 
 module.exports = pool;
